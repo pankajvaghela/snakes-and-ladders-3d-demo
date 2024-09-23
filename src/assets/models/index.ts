d3ea0127f9bf0fb3a66fdebe208d0faf44fb6d8f
Pankaj Vaghela <vpankaj1998official@gmail.com>
2024-09-23 15:45:40 +0200

    Add model assets

---
+++ src/assets/models/index.ts
@@ -0,0 +1,33 @@
+import GameBoard1 from './game-board.glb';
+import GameBoard2 from './game-board2.glb';
+import Ladder1 from './ladder1.glb';
+import Ladder2 from './ladder2.glb';
+import Ladder3 from './ladder3.glb';
+import Ladder4 from './ladder4.glb';
+import Ladder5 from './ladder5.glb';
+import Snake1 from './snake1.glb';
+import Snake2 from './snake2.glb';
+import Snake3 from './snake3.glb';
+import Snake4 from './snake4.glb';
+import Snake5 from './snake5.glb';
+import Snake6 from './snake6.glb';
+import Dice from './dice.glb';
+import Dice2 from './dice2.glb';
+
+export {
+  GameBoard1,
+  GameBoard2,
+  Dice,
+  Dice2,
+  Ladder1,
+  Ladder2,
+  Ladder3,
+  Ladder4,
+  Ladder5,
+  Snake1,
+  Snake2,
+  Snake3,
+  Snake4,
+  Snake5,
+  Snake6,
+};
