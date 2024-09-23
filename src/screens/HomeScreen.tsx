8dfcb4d3611e342e9e9771cf78811b9e0627efb4
Pankaj Vaghela <vpankaj1998official@gmail.com>
2024-09-23 15:43:47 +0200

    Add navigation

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export function HomeScreen() {
+  return (
+    <View style={{flex: 1}}>
+      <Text
+        style={{
+          fontSize: 20,
+          color: 'black',
+        }}>
+        Game will be added here
+      </Text>
+    </View>
+  );
+}
